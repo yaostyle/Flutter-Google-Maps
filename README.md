52f7d1da6ff00b14d3754ba185d1341279a86f40
Chris Hsu <yaostyle@gmail.com>
2019-05-01 16:14:07 -0700

    Added marker option and move camera position.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Flutter Google Maps
 
-A quick Google Maps tutorial practice from this guy [Youtube - Raja Yogan](https://youtu.be/Egp_S8JN-ec). 
-Thank you and keep up the good work!
+A quick Google Maps tutorial practice based on [Youtube - Raja Yogan](https://youtu.be/Egp_S8JN-ec)'s video and 
+updated docs on https://pub.dartlang.org/packages/google_maps_flutter.
+  
+Thanks, Raja for the inspiration. 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Google Maps Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -26,10 +29,95 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //GoogleMapController myController;
+  Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center =
+      const LatLng(37.42796133580664, -122.085749655962);
+  final Set<Marker> _markers = {};
+  LatLng _lastMapPosition = _center;
+
+  static final CameraPosition initCameraPosition = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  void _pinHere() {
+    setState(() {
+      _markers.add(Marker(
+        markerId: MarkerId(_lastMapPosition.toString()),
+        position: _lastMapPosition,
+        infoWindow: InfoWindow(
+          title: 'Hello here',
+          snippet: 'Super!',
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+  }
+
+  void _onCamMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            mapType: MapType.hybrid,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+            initialCameraPosition: initCameraPosition,
+            compassEnabled: true,
+            markers: _markers,
+            onCameraMove: _onCamMove,
+          ),
+          SizedBox(
+            child: Center(
+              child: Icon(
+                Icons.add_location,
+                size: 40.0,
+                color: Colors.pink[600],
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: FloatingActionButton.extended(
+                onPressed: _goToTheLake,
+                icon: Icon(Icons.directions_boat),
+                label: Text('To the lake!')),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: FloatingActionButton.extended(
+              onPressed: _pinHere,
+              icon: Icon(Icons.add_location),
+              label: Text('Pin Here'),
+              backgroundColor: Colors.pink,
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
-    return Scaffold();
-
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
